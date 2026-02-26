d34c715f13504a8c51286eb25bf0d6eec6595790
caduoliveira-dev <ribeirocarlosolivera@gmail.com>
2026-02-26 00:04:39 -0300

    fix: correção da migration de criação da tabela de produtos

--- src/main/resources/db/migration/V2__create-product.sql
+++ src/main/resources/db/migration/V2__create-product.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE raw_material (
+CREATE TABLE product (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     price INTEGER NOT NULL
